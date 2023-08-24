eb525dded51a7290d6126fa205bc607907f700f6
Aayush <aayushv@heaptrace.net>
2023-08-24 16:34:25 +0530

    commit

--- firstProject.java
+++ firstProject.java
@@ -4,7 +4,6 @@ public class firstProject {
     public static void main(String[] args) {
         System.out.println("Git is officially defined as a distributed version control system (VCS).");
         System.out.println("it's a system that tracks changes to our project files over time");
-        System.out.println(
-                "It enables us to record project changes and go back to a specific version of the tracked files, at any given point in time");
+
     }
 }

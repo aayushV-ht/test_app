9142948c709a644ccf53298a732b3dc3958b1bc1
Aayush <aayushv@heaptrace.net>
2023-08-24 16:44:35 +0530

    new branch creat and change made

--- firstProject.java
+++ firstProject.java
@@ -2,9 +2,11 @@ package test_app;
 
 public class firstProject {
     public static void main(String[] args) {
-        System.out.println("Git is officially defined as a distributed version control system (VCS).");
-        System.out.println("it's a system that tracks changes to our project files over time");
-        System.out.println(
-                "It enables us to record project changes and go back to a specific version of the tracked files, at any given point in time");
+        System.out.println("This system can be used by many people to efficiently work together.");
+        System.out.println("collaborate on team projects,");
+        System.out.println("where each developer can have their own version of the project,");
+        System.out.println("distributed on their computer");
+        System.out.println("Later on, these individual versions of the project can be merged ");
+        System.out.println("nd adapted into the main version of the project.");
     }
 }
